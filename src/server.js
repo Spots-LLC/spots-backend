b1575ea5205820dbbf78fa289f27e12a8115fec7
brosmar18 <bryangonzalez1896@icloud.com>
2024-01-05 17:20:23 -0600

    server errorHandler implemented in server.js. Successful when tested on thunderclient

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const logger = require('./utils/logger');
+
+// error handlers
 const notFound = require('./handlers/404');
+const errorHandler = require('./handlers/500');
 
 require('dotenv').config();
 const PORT = process.env.PORT || 5002;
@@ -12,7 +15,12 @@ app.get('/', (req, res, next) => {
     res.status(200).send('Hello World!');
 });
 
+app.get('/error', (req, res, next) => {
+    throw new Error('Forced Error for Testing');
+});
+
 app.use('*', notFound);
+app.use(errorHandler);
 
 const start = () => {
     app.listen(PORT, () => {
