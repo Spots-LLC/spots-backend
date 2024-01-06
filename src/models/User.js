ccf9e7c4f7989460d417955fbde2d57856f8aff2
brosmar18 <bryangonzalez1896@icloud.com>
2024-01-05 22:23:03 -0600

    User model created

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+    },
+    role: {
+        type: [String],
+        required: true,
+        enum: ['attendee', 'organizer'],
+    },
+    location: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
